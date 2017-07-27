96b55fc5771718f01498d808fc3661fca3932f4a
BradM <bradm@inmotionhosting.com>
2017-07-27 14:15:20 -0400

    (8) travis-ci setup

--- tests/test-name.php
+++ tests/test-name.php
@@ -3,7 +3,22 @@
 //
 
 class Test_Name extends WP_UnitTestCase {
+	/**
+	 *
+	 */
+	public function testCreateUnique() {
+		$name = "Catfish Menu";
 
+		// This menu should not exist.
+		$uniqueName = \BoldGrid\WpMenuHelpers\Name::createUnique( $name );
+		$this->assertSame( $name, $uniqueName );
+
+		$menuId = wp_create_nav_menu( $uniqueName );
+
+		// The menu now exists, so the next name should appended with -1.
+		$uniqueName = \BoldGrid\WpMenuHelpers\Name::createUnique( $name );
+		$this->assertSame( $uniqueName, $name . '-1' );
+	}
 }
 
 ?>
