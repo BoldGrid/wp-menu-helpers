0b133c1370042496b50d508c9e0bbd79927cd573
BradM <bradm@inmotionhosting.com>
2017-07-27 14:53:16 -0400

    (16) travis-ci setup

--- tests/test-name.php
+++ tests/test-name.php
@@ -36,13 +36,11 @@ class Test_Name extends WP_UnitTestCase {
 		$this->assertSame( $name, $uniqueName );
 
 		$menuId = wp_create_nav_menu( $uniqueName );
-
 		// The menu now exists, so the next name should appended with -2.
 		$uniqueName = \BoldGrid\WpMenuHelpers\Name::createUnique( $name );
 		$this->assertSame( $uniqueName, $name . '-2' );
 
 		$menuId = wp_create_nav_menu( $uniqueName );
-
 		$uniqueName = \BoldGrid\WpMenuHelpers\Name::createUnique( $name );
 		$this->assertSame( $uniqueName, $name . '-3' );
 
@@ -50,7 +48,7 @@ class Test_Name extends WP_UnitTestCase {
 		 * Make sure -2 through -100 menu names exist. After this limit,
 		 * createUnique will return false.
 		 */
-		for( $x = 4; $x <= 100; $x++ ) {
+		for( $x = 3; $x <= 100; $x++ ) {
 			wp_create_nav_menu( $name . '-' . $x );
 		}
 		$uniqueName = \BoldGrid\WpMenuHelpers\Name::createUnique( $name );
